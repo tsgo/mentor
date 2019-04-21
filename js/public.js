dce9f7e4154d20a2bea5527b65cbd8e86a68f4b7
SaSaChu <teresa7397@gmail.com>
2019-04-21 15:48:23 +0800

    登入頁＋內頁架構完成

--- js/public.js
+++ js/public.js
@@ -1,18 +1,34 @@
 
 $(function () {
 
-  $('.c_m').each(function() { //each的function = 這個樣式下面的每一個東西，都執行下列語法
-
-    var L = $(this).find ('>.a1').length; //抓取這個樣式，第一層 叫 .a1 元素的長度(有幾個)
-    var c = 'na' + L; //na是對應scss中，我給他的變數名稱，這個名稱＋元素有幾個（上面var的內容）
-
-    $(this).addClass(c); // .c_m 樣式，就加上var c 計算出來的總數
-
-    $(this).prev().click(function() { // .c_m 樣式的父親層（ prev() ）被點擊的時候，執行下面動作。
-      // $('.c_m').removeClass ('open');
-      $(this).next().toggleClass ('open'); // 這段用意是指定點擊該選項，才會觸發該選項下方的盒子。
-    });
+  // 手機選單
+  $('.menu, .menu_boxs .icon-x').click(function() {
+    $('.menu_boxs').toggleClass ('hovermenu');
+    $('.cover').toggleClass('show');
   });
 
+  $('.threeMenu').click(function() {
+    $('.lastMenu_boxs').toggleClass ('showMenu');
+  });
+
+  // 手機選單模式，背景資訊鎖定
+  function scrollLock () {
+    var scrollPosition = [
+      self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+      self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+    ];
+    var $html = $('html, body'); // it would make more sense to apply this to body, but IE7 won't have that
+    $html.data('scroll-position', scrollPosition);
+    $html.data('previous-overflow', $html.css('overflow'));
+    $html.css('overflow', 'hidden');
+    window.scrollTo(scrollPosition[0], scrollPosition[1]);
+  }
+
+  function scrollUnlock () {
+    var $html = $('html, body'); // it would make more sense to apply this to body, but IE7 won't have that
+    var scrollPosition = $html.data('scroll-position');
+    $html.css('overflow', $html.data('previous-overflow'));
+    window.scrollTo(scrollPosition[0], scrollPosition[1]);
+  }
 
 });
